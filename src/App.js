383d8ba6b7acc224c61c19bffd6d0191d2849a85
Dale 'Ducky' Lotts <dlotts@knightrider.com>
2020-05-07 20:31:09 -0400

    feat(Home): add home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import {Home} from "./home/Home";
 
 function App() {
   return (
-    <Home />
+    <Home name="Dale Lottsssssss" />
   );
 }
 
